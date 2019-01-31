bd0c4e405f840dcf8675bfe0716d9d950570d298
Fluxayt123 <47192617+Fluxayt123@users.noreply.github.com>
2019-01-31 15:04:56 +0100

    Update ready.js

--- ready.js
+++ ready.js
@@ -6,7 +6,7 @@ module.exports = bot => {
 
     console.log(`${bot.user.username} is online!`)
     
-    var playing = [`over ${bot.users.size} users!`, "*FNPS", "help", `FN-Scrims`]
+    var playing = [`over ${bot.users.size} users!`, "*FNPS", "*help", `FN-Scrims`]
     var interval = setInterval(function() {
         var game = Math.floor((Math.random() * playing.length) + 0);
         bot.user.setGame(playing[game])
